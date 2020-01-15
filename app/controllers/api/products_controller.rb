a790ddbe72823d56bdde3c11dbc1731ab3d60771
sbatsch <sbatsch2@gmail.com>
2020-01-15 11:19:17 -0600

    added products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,16 @@
 class Api::ProductsController < ApplicationController
+  def first_product
+    @product = Product.first
+    render "first_product.json.jb"
+  end 
+
+  def all_products
+    @products = Product.all
+    render "all_products.json.jb"
+  end 
 end
+
+
+
+
+ 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
-
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+ namespace :api do
+  get '/first_product' => 'products#first_product'
+  get '/all_products' => 'products#all_products'
+ end 
 end

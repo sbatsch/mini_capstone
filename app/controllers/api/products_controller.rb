26cfbc93be1a6cadd7791ae6539d227fdefba25d
sbatsch <sbatsch2@gmail.com>
2020-01-17 11:27:28 -0600

    Added destroy

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -4,10 +4,38 @@ class Api::ProductsController < ApplicationController
     render 'index.json.jb'
   end 
 
+  def create 
+    @product = Product.new(name: params[:name],
+                           price: params[:price],
+                           image_url: params[:image_url],
+                           description: params[:description]
+                          )
+    @product.save
+    render 'show.json.jb'
+  end
+
   def show
     @product = Product.find(params[:id])
     render 'show.json.jb'
   end 
+
+  def update
+    @product = Product.find(params[:id])
+
+    @product.name = params[:name]  || @product.name
+    @product.price = params[:price]  || @product.price
+    @product.image_url = params[:image_url] || @product.image_url
+    @product.description = params[:description] || @product.description
+
+    @product.save 
+    render 'show.json.jb'
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    render json: {message: "Product sucessfully destroyed."}
+  end
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
- namespace :api do
-  get '/products' => 'products#index'
-  get '/products/:id' => 'products#show'
- end 
+   namespace :api do
+    get '/products' => 'products#index'
+    post '/products' => 'products#create'
+    get '/products/:id' => 'products#show'
+    patch 'products/:id' =>'products#update'
+    delete '/products/:id' => 'products#destroy'
+   end 
 end

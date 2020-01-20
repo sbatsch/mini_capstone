532af8b1178235648485fb88316ddc7f91d084b3
sbatsch <sbatsch2@gmail.com>
2020-01-20 12:08:13 -0600

    partial

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,18 @@
 class Product < ApplicationRecord
+  def is_discounted?
+    if self.price > 10000
+      return false 
+    else 
+      return true
+    end 
+  end 
+
+  def tax
+    return self.price * 0.09 
+  end 
+
+  def total
+    return self.price + self.tax
+  end
+
 end

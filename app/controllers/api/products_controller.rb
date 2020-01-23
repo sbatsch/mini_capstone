b17c2e1c2694e2afb6656fcd512e37999a4c0ecc
sbatsch <sbatsch2@gmail.com>
2020-01-23 13:37:02 -0600

    Updated

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,29 @@
 class Api::ProductsController < ApplicationController
   def index
     @products = Product.all 
+
+    search_term = params[:search]
+    discount_option = params[:dsicount] == "true"
+    sort_attribute = params[:sort]
+    sort_order = params[:sort_order]
+
+    if search_term
+      @products = Product.where("name iLIKE ?", "%#{ search_term }%")
+    end 
+
+    if discount_option
+      @products = Product.all.where("price < ?", 10000)
+    end 
+
+    if sort_attribute == "price" && sort_order == "desc"
+      @products = @products.order(price: :desc)
+    elsif sort_attribute == "price"
+      @products = @products.order(price: :asc)
+    else 
+      @products = @products.order(:id) 
+    end 
+
+
     render 'index.json.jb'
   end 
 
@@ -43,8 +66,3 @@ class Api::ProductsController < ApplicationController
     render json: {message: "Product sucessfully destroyed."}
   end
 end
-
-
-
-
- 

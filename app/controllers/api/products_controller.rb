87763e2e80c3d40acb4526b930fd9cd143aaec5f
sbatsch <sbatsch2@gmail.com>
2020-01-27 10:36:28 -0600

    Updated

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -30,8 +30,8 @@ class Api::ProductsController < ApplicationController
   def create 
     @product = Product.new(name: params[:name],
                            price: params[:price],
-                           image_url: params[:image_url],
-                           description: params[:description]
+                           description: params[:description],
+                           supplier_id: params[:supplier_id]
                           )
     if @product.save
       render 'show.json.jb'
@@ -50,8 +50,8 @@ class Api::ProductsController < ApplicationController
 
     @product.name = params[:name]  || @product.name
     @product.price = params[:price]  || @product.price
-    @product.image_url = params[:image_url] || @product.image_url
     @product.description = params[:description] || @product.description
+    @product.supplier_id = params[:supplier]_id || @product.supplier_id
 
     if @product.save 
       render 'show.json.jb'
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+  has_many :images
   belongs_to :supplier
 
   # def supplier

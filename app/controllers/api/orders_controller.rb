acc179b09f2bf57d011590d7d03dd1c2f37a2817
sbatsch <sbatsch2@gmail.com>
2020-01-29 11:45:39 -0600

    created orders with model methods

---
+++ app/controllers/api/orders_controller.rb
@@ -0,0 +1,43 @@
+class Api::OrdersController < ApplicationController
+  def index
+    if current_user
+      @orders = current_user.orders 
+      render 'index.json.jb'
+    else
+      render json: []
+    end 
+  end 
+
+  def create 
+    product = Product.find(params[:product_id])
+    calculated_subtotal = product.price * params[:quantity].to_i
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total = calculated_subtotal + calculated_tax
+  
+
+
+    @order = Order.new(
+                       user_id: current_user.id, 
+                       product_id: params[:product_id],
+                       quantity: params[:quantity],
+                       subtotal: calculated_subtotal, 
+                       tax: calculated_tax,
+                       total: calculated_total
+                      )
+
+    if @order.save
+      render 'show.json.jb'
+    else
+      render json: {errors: @order.errors.full_messages}, status: :unprocessable_entity
+    end 
+  end
+
+  def show
+    if current_user
+      @order = Order.find(params[:id])
+      render 'show.json.jb'
+    else
+      render json: {}
+    end 
+  end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
    namespace :api do
+    get "/orders" => 'orders#index'
+    post "/orders" => "orders#create"
+    get '/orders:id' => "orders#show"
+
     post "/sessions" => "sessions#create"
+
     post "/users" => "users#create"
+
     get '/products' => 'products#index'
     post '/products' => 'products#create'
     get '/products/:id' => 'products#show'
+    
     patch 'products/:id' =>'products#update'
     delete '/products/:id' => 'products#destroy'
    end 
